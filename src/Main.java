f1239fca70e1f873561a676044a51b1861dff3ac
GabrielDPimentel <fodasedfgh@gmail.com>
2025-08-17 18:41:39 -0300

    Enviando arquivo, praticando polimorfismo.

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of employees : ");
+        int n = sc.nextInt();
+        ArrayList<Employee> employees = new ArrayList<>();
+
+        for(int i = 0; i<n; i++){
+            System.out.println("Employee # " + (i + 1));
+            System.out.print("Outsourced ? (y/n):  " );
+            char question = sc.next().charAt(0);
+                System.out.print("Name:" );
+                String name = sc.nextLine();
+                System.out.print("Hours: ");
+                int hours = sc.nextInt();
+                System.out.print("Value per hour: ");
+                double valuePerHour = sc.nextDouble();
+
+                if(question == 'y'){
+                    System.out.print("Additional charge : ");
+                    double additionalCharge = sc.nextDouble();
+                    employees.add(new OutsourcedEmployee(name,hours,valuePerHour,additionalCharge));
+                } else {
+                    employees.add(new Employee(name,hours,valuePerHour));
+                }
+            }
+        System.out.println();
+        System.out.println("PAYMENTS: ");
+
+        for(Employee emp : employees){
+            System.out.println(emp.getName() + "- $ " + String.format("%.2f", emp.payment()));
+
+
+        }
+    }
+
+
+
+
+}
---
+++ src/entities/OutsourcedEmployee.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class OutsourcedEmployee extends Employee {
+
+    private Double additionalCharge;
+
+    public OutsourcedEmployee(){}
+
+    public OutsourcedEmployee(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+    public OutsourcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+        super(name, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+    @Override
+    public double Payment(int hours, double valuePerHour){
+        return (super.Payment(hours,valuePerHour) + additionalCharge) * 1.1 ;
+    }
+}
